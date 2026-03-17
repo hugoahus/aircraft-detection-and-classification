7852c35847dab1d3f782429b9a648c7a036924a1
hugolw <hugolw@kth.se>
2026-03-17 00:04:04 -0400

    Initial training of model, relates to #3

--- doc/notebooks/config.py
+++ doc/notebooks/config.py
@@ -1,20 +1,21 @@
 CONFIG = {
-    "model": "yolo11n.pt",
+    "model": "yolo11m.pt",           
     "data": "../../dataset.yaml",
     "imgsz": 320,
 
-    # Hyperparameters
-    "epochs": 5,
-    "batch": 1,
-    "optimizer": "SGD",
-    "lr0": 1e-3,
-    "lrf": 0.01,
-    "momentum": 0.937,
-    "weight_decay": 5e-4,
-
-    # Default
-    "device": "cpu",
+    # Hyperparameters             
+    "epochs": 100,                   
+    "batch": 32,                     
+    "optimizer": "AdamW",            
+    "lr0": 0.01,                     
+    "lrf": 0.01,                     
+    "patience": 20,                  
+    "save_period": 5,
+    "weight_decay": 0.0005,          
+    "device": 0,                     
     "project": "runs/train",
-    "name": "exp1",
-    "exist_ok": False,
+    "name": "aircraft_yolo11m_v1",
+    "exist_ok": True,
+    "plots": True,                  
+    "mosaic": 0.0                
 }
